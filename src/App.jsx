8ff2ba99d6b20a5af0b67b9dcbc2feebb809111d
NicoAJS <118994793+NicoAJS@users.noreply.github.com>
2024-04-16 10:51:42 +0200

    Nico test 16/04 kl. 10:51

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Sidehoved from './components/Sidehoved'
+import cards from './components/Cards'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Sidehoved />
+      <cards />
     </>
   )
 }
---
+++ src/components/Cards.jsx
@@ -0,0 +1,33 @@
+export default function cards() {
+
+    return(
+        <>
+        <div className="allcards">
+
+            <div class="card">
+                <h3>Title</h3>
+                <img src="" alt="" />
+                <h5>Under title</h5>
+                <p>Brød tekst: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ultrices tincidunt arcu non.</p>
+            </div>
+
+            <div class="card">
+                <h3>Title</h3>
+                <img src="" alt="" />
+                <h5>Under title</h5>
+                <p>Brød tekst: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ultrices tincidunt arcu non.</p>
+            </div>
+
+            <div class="card">
+                <h3>Title</h3>
+                <img src="" alt="" />
+                    <h5>Under title</h5>
+                    <p>Brød tekst: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ultrices tincidunt arcu non.</p>
+            </div>
+
+        </div>
+
+
+        </>
+    )
+}
