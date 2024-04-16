f801c45777d3cccb9ff8189101ee448a1221a417
nikolinef <126453544+nikolinef@users.noreply.github.com>
2024-04-16 10:58:13 +0200

    kortupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Sidehoved from './components/Sidehoved'
-import cards from './components/Cards'
+import Cards from './components/Cards'
 import './App.css'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Sidehoved />
-      <cards />
+      <Cards />
     </>
   )
 }
