b0ccde6fddb20cf3f213e969d49b3c9986ff8e26
nikolinef <126453544+nikolinef@users.noreply.github.com>
2024-04-17 15:33:45 +0200

    folders styleupdate

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -2,20 +2,49 @@ import React from "react";
 import folderpil from "../assets/folderpilned.svg";
 import Beandafter from "./Beandafter";
 
-function Folder({title, folderdescrib}){
+function Folder(){
 return (
-    <div className="onefolder">
-        <div className="folderheadline">
-            <h2>{title}</h2>
-            <img src={folderpil} className="folderarrow" alt="Pil" />
+    <>
+        <div className="onefolder" style={{backgroundColor: "#B9C186", color: "#412F26"}}>
+            <div className="folderheadline">
+                <h2>Pakke 1 - Billedeoptimering</h2>
+                <img src={folderpil} className="folderarrow" alt="Pil" />
+            </div>
+            <div className="foldercontent">
+                <p>I denne pakke går vi ind og reducerer storage af billeder og video. Dvs. vi komprimere alle filstørrelserne, så de fylder væsentlig mindre, men stadig bevære kvaliteten. Derudover kan vi også lave en test på billedernes nødvendighed, så vi sikre, at i kun har nødvendige billeder på jeres side</p>
+                <h3>Knap</h3>
+                <Beandafter
+                />
+            </div>
         </div>
-        <div className="foldercontent">
-            <p>{folderdescrib}</p>
-            <h3>Knap</h3>
-            <Beandafter
-            />
+
+        <div className="onefolder" style={{backgroundColor: "#6A6F4C", color: "#EDE1D2"}}>
+            <div className="folderheadline">
+                <h2>Pakke 2 - Designoptimering</h2>
+                <img src={folderpil} className="folderarrow" alt="Pil" />
+            </div>
+            <div className="foldercontent">
+                <p>I denne pakke går vi ind og reducerer storage af billeder og video. Dvs. vi komprimere alle filstørrelserne, så de fylder væsentlig mindre, men stadig bevære kvaliteten. Derudover kan vi også lave en test på billedernes nødvendighed, så vi sikre, at i kun har nødvendige billeder på jeres side</p>
+                <h3>Knap</h3>
+                <Beandafter
+                />
+            </div>
         </div>
-    </div>
+
+        <div className="onefolder" style={{backgroundColor: "#806044", color: "#EDE1D2"}}>
+            <div className="folderheadline">
+                <h2>Pakke - Rebranding</h2>
+                <img src={folderpil} className="folderarrow" alt="Pil" />
+            </div>
+            <div className="foldercontent">
+                <p>I denne pakke går vi ind og reducerer storage af billeder og video. Dvs. vi komprimere alle filstørrelserne, så de fylder væsentlig mindre, men stadig bevære kvaliteten. Derudover kan vi også lave en test på billedernes nødvendighed, så vi sikre, at i kun har nødvendige billeder på jeres side</p>
+                <h3>Knap</h3>
+                <Beandafter
+                />
+            </div>
+        </div>
+    </>
+    
 
 
 )
--- src/components/Folders.jsx
+++ src/components/Folders.jsx
@@ -4,24 +4,10 @@ export default function Folders() {
 
     return(
         <>
-            <h1>Pakker vi tilbyder</h1>
+            <h1 style={{textAlign: "center", padding: "2dvh 0", color: "#412F26"}}>Pakker vi tilbyder</h1>
             <div className="folders">
-                <Folder 
-                title="Pakke 1 - Billedeoptimering"
-                folderdescrib="I denne pakke går vi ind og reducerer storage af billeder og video. Dvs. vi komprimere alle filstørrelserne, så de fylder væsentlig mindre, men stadig bevære kvaliteten. Derudover kan vi også lave en test på billedernes nødvendighed, så vi sikre, at i kun har nødvendige billeder på jeres side"
-                
+                <Folder
                 />
-
-                <Folder 
-                title="Pakke 2 - Designoptimering"
-                folderdescrib="I denne pakke går vi ind og reducerer storage af billeder og video. Dvs. vi komprimere alle filstørrelserne, så de fylder væsentlig mindre, men stadig bevære kvaliteten. Derudover kan vi også lave en test på billedernes nødvendighed, så vi sikre, at i kun har nødvendige billeder på jeres side"
-                />
-
-                <Folder 
-                title="Pakke 3 - Rebranding"
-                folderdescrib="I denne pakke går vi ind og reducerer storage af billeder og video. Dvs. vi komprimere alle filstørrelserne, så de fylder væsentlig mindre, men stadig bevære kvaliteten. Derudover kan vi også lave en test på billedernes nødvendighed, så vi sikre, at i kun har nødvendige billeder på jeres side"
-                style={{ backgroundColor: '#806044' }}
-                /> 
             </div>
         </>
     )
