0ee449ca38fc451b2b9bcac12cad9f0305f23443
NicoAJS <118994793+NicoAJS@users.noreply.github.com>
2024-04-17 13:49:48 +0200

    cards 1

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,3 +1,5 @@
+import placeholderImg from '../assets/noimg.png'
+
 export default function Cards() {
 
     return(
@@ -6,23 +8,23 @@ export default function Cards() {
 
             <div className="card">
                 <h3>Title</h3>
-                <img src="" alt="" />
+                <img src={placeholderImg} alt="Placeholder img" />
                 <h5>Under title</h5>
                 <p>Brød tekst: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ultrices tincidunt arcu non.</p>
             </div>
 
             <div className="card">
                 <h3>Title</h3>
-                <img src="" alt="" />
+                <img src={placeholderImg} alt="Placeholder img" />
                 <h5>Under title</h5>
                 <p>Brød tekst: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ultrices tincidunt arcu non.</p>
             </div>
 
             <div className="card">
                 <h3>Title</h3>
-                <img src="" alt="" />
-                    <h5>Under title</h5>
-                    <p>Brød tekst: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ultrices tincidunt arcu non.</p>
+                <img src={placeholderImg} alt="Placeholder img" />
+                <h5>Under title</h5>
+                <p>Brød tekst: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ultrices tincidunt arcu non.</p>
             </div>
 
         </div>
