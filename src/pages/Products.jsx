e658408be592b16065eed3dae3e8a8fc5eac337a
nikolinef <126453544+nikolinef@users.noreply.github.com>
2024-04-18 17:08:34 +0200

    v18/04 kl. 17:08

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,81 @@
 import Card from "../components/Cards";
 import noImg from "../assets/noimg.png";
 import Leasbtn from "../components/Leasbtn";
+import Folderdescribtion from "../components/Folderdescribtion";
+import Beforeandafter from "../components/Beforeandafter";
 
 export default function Products() {
 
     return (
         <>
+        <div className="onefolder" style={{backgroundColor: "#B9C186", color: "#412F26"}}>
+          <div className="folderhealine">
+            <h2>Pakke 1 - Billedeoptimering</h2>
+          </div>
+
+          <div className="foldercontent">
+            <Folderdescribtion
+            fdescribtion="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+            Proin nibh nisl condimentum id venenatis. Elementum tempus egestas sed sed risus pretium quam vulputate. 
+            
+            Porttitor leo a diam sollicitudin tempor id. Amet justo donec enim diam vulputate. Ipsum nunc aliquet bibendum enim."
+            />
+            <h3>Knap</h3>
+            <Beforeandafter
+            lagringspladspre="14,07 GB"
+            kgwattpre="105 kg/watt"
+            lagringspladspost="504 KB"
+            kgwattpost="20 kg/watt"
+            />
+          </div>
+        </div>
+
+        <div className="onefolder" style={{backgroundColor: "#6A6F4C", color: "#EDE1D2"}}>
+          <div className="folderhealine">
+            <h2>Pakke 2 - Designoptimering</h2>
+          </div>
+
+          <div className="foldercontent">
+            <Folderdescribtion
+            fdescribtion="Design beskrivelse Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+            Proin nibh nisl condimentum id venenatis. Elementum tempus egestas sed sed risus pretium quam vulputate. 
+            
+            Porttitor leo a diam sollicitudin tempor id. Amet justo donec enim diam vulputate. Ipsum nunc aliquet bibendum enim."
+            />
+            <h3>Knap</h3>
+            <Beforeandafter
+            lagringspladspre="17,07 GB"
+            kgwattpre="150 kg/watt"
+            lagringspladspost="378 KB"
+            kgwattpost="17 kg/watt"
+            />
+          </div>
+        </div>
+
+        <div className="onefolder" style={{backgroundColor: "#806044", color: "#EDE1D2"}}>
+          <div className="folderhealine">
+            <h2>Pakke 3 - Rebranding</h2>
+          </div>
+
+          <div className="foldercontent">
+            <Folderdescribtion
+            fdescribtion="Rebranding beskrivelse beskrivelse Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+            Proin nibh nisl condimentum id venenatis. Elementum tempus egestas sed sed risus pretium quam vulputate. 
+            
+            Porttitor leo a diam sollicitudin tempor id. Amet justo donec enim diam vulputate. Ipsum nunc aliquet bibendum enim."
+            />
+            <h3>Knap</h3>
+            <Beforeandafter
+            lagringspladspre="20,56 GB"
+            kgwattpre="289 kg/watt"
+            lagringspladspost="482 KB"
+            kgwattpost="10 kg/watt"
+            />
+          </div>
+        </div>
     
     <h3 className="centerTekst">Example Component</h3>
     <div className="allcards">
