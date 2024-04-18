6c6ac9530baa03d54340f458b64e68fb7cfc1b3c
nikolinef <126453544+nikolinef@users.noreply.github.com>
2024-04-18 21:24:24 +0200

    v18/04 kl. 21:24

--- src/components/Costumerrequest.jsx
+++ src/components/Costumerrequest.jsx
@@ -4,7 +4,25 @@ function Costumerrequest() {
     return(
         <>
         <div className="costumerrequest">
-            
+            <label htmlFor="name">Navn/firmanavn:</label>
+            <input type="text" 
+            name="navn"
+            placeholder="Navn/firmanavn"/>
+
+            <label htmlFor="name">Mobilnummer:</label>
+            <input type="number" 
+            name="mobilnummer"
+            placeholder="Mobilnummer"/>
+
+            <label htmlFor="name">E-mail:</label>
+            <input type="email" 
+            name="email"
+            placeholder="E-mail"/>
+
+            <label htmlFor="name">Besked:</label>
+            <input type="text" 
+            name="besked"
+            placeholder="Forespørgsel til mødetidspunkt, dato og evt. sted"/>
             
         </div>
         </>
