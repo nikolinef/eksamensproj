62268949d38105bef3e918da91449114c844179e
nikolinef <126453544+nikolinef@users.noreply.github.com>
2024-04-18 18:49:15 +0200

    header m. logo

--- src/components/Sidehoved.jsx
+++ src/components/Sidehoved.jsx
@@ -1,11 +1,16 @@
 import { NavLink } from "react-router-dom";
+import Mlogo from "../assets/SWDlogo.svg";
 
 export default function Sidehoved() {
 
     return(
         <>
-        <nav>
-        <NavLink to="/"><div className="headerlogo">Forside</div></NavLink>
+        <nav className="nav">
+        <NavLink to="/">
+            <div className="headerlogo">
+                <img src={Mlogo} alt="defaultpic" className="headerimg"/>
+            </div>
+        </NavLink>
 
             <div className="menu">
                 <NavLink to="/products" className="NavLink">Produkter</NavLink>
