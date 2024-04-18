c9dd3f8376f3880ce8d0bf2b41f4abaf8b9b4740
NicoAJS <118994793+NicoAJS@users.noreply.github.com>
2024-04-18 12:34:07 +0200

    Homepage updatering (18/04 kl. 12:33)

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,5 @@
 import Centertekst from "../components/Centertekst"
 import Leasbtn from "../components/Leasbtn"
-import CentertekstBtn from "../components/CentertekstBtn"
 import Card from "../components/Cards";
 import noImg from "../assets/noimg.png";
 
@@ -13,13 +12,10 @@ export default function Homepage() {
         <>
         <Centertekst overskrift="Velkommen til Evergreen" brodtekst="Lille velkomst tekst, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
 
-        <div className="centertekstbtn">
-                <CentertekstBtn
-                    overskrift="Hvem er Evergreen"
-                    brodtekst="Hvad er vores mission og vission, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                />
-                <Leasbtn link="Der hvor du gerne vil hen med linket" />
-            </div>
+        <Centertekst overskrift="Hvem er Evergreen" brodtekst="Introduction om hvem vi er og hvad vi står for, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+
+        <Leasbtn link="/about">Læs mere</Leasbtn>
+
 
         <h3 className="underOverskrift">Example Component</h3>
         <div className="allcards">
