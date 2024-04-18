aade6d347df474a39651f0d27ac69ce4b0cb4e07
NicoAJS <118994793+NicoAJS@users.noreply.github.com>
2024-04-18 16:31:43 +0200

    About (18/04 kl. 16:31)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,6 +6,27 @@ export default function About() {
         <>
         <Stortlogo />
         
+        <div className="leftTekst">
+            <h2 className="leftTekst">Hvem er Evergreen</h2>
+            <p className="leftTekst">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <br />
+                Proin nibh nisl condimentum id venenatis. Elementum tempus egestas sed sed risus pretium quam vulputate. 
+                <br />
+                Porttitor leo a diam sollicitudin tempor id. Amet justo donec enim diam vulputate. Ipsum nunc aliquet bibendum enim.</p>
+        </div>
+
+
+        <div className="leftTekst">
+            <h2 className="leftTekst">Hvad er bæredygtigt webdesign? og hvorfor er det vigtigt</h2>
+            <p className="leftTekst">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <br />
+                A cras semper auctor neque vitae tempus quam. Cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Arcu risus quis varius quam quisque id.
+                <br />
+                Placerat vestibulum lectus mauris ultrices eros. Fermentum odio eu feugiat pretium.
+            </p>
+        </div>
         
         
         </>
