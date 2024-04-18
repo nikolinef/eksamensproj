9e3f3e054a6942d7b25027cd46e4d5089420eda5
NicoAJS <118994793+NicoAJS@users.noreply.github.com>
2024-04-18 13:06:40 +0200

    Homepage update (18/04 kl. 13:06)

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -17,44 +17,46 @@ export default function Homepage() {
 
         <Leasbtn link="/about">Læs mere</Leasbtn>
 
-        <Funfact
-        overskrift="Fun Fact 1"
-        brodtekst="Some paragraph text: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <Leasbtn link="/about">Læs mere</Leasbtn>
 
-        <Funfact
-        overskrift="Fun Fact 2"
-        brodtekst="Some paragraph text: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <Leasbtn link="/about">Læs mere</Leasbtn>
-
-
-        <h3 className="underOverskrift">Example Component</h3>
+        <h3 className="underOverskrift">Vores pakker</h3>
         <div className="allcards">
             <Card
-            title="Firma Navn 1"
+            title="Pakke 1"
             image={noImg} // Use noImg as the image prop
             alt="Example Image"
             underTitle="Billedeoptimering"
             paragraph="Tester tester tester."
         />
             <Card
-            title="Firma Navn 2"
+            title="Pakke 2"
             image={noImg}
             alt="Example Image"
             underTitle="Designoptimering"
             paragraph="This is an example paragraph."
         />
             <Card
-            title="Firma Navn 3"
+            title="Pakke 3"
             image={noImg}
             alt="Example Image"
             underTitle="Rebranding"
             paragraph="This is an example paragraph."
         />
 
-        </div>    
+        </div> 
+
+        <Funfact
+            overskrift="Fact 1"
+            brodtekst="Brødtekst"
+        />
+        <Leasbtn link="/about">Læs mere</Leasbtn>
+
+        <Funfact
+            overskrift="Fact 2"
+            brodtekst="Brødtekst"
+        />
+        <Leasbtn link="/about">Læs mere</Leasbtn>
+
+        <h3 className="underOverskrift">Vores tidliger kunder</h3>
         
         </>
     )
