172b7d551f9e529c51db3f559f7e1f729c5c50df
nikolinef <126453544+nikolinef@users.noreply.github.com>
2024-04-21 20:44:17 +0200

    firebasestart

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -28,7 +28,7 @@ export default function Contact() {
             fdescribtion="Til mødet vil vi sammen snakke om de forskellige optimeringsmuligheder og behov, din virksomhed har, og vil fastlægge et forløb der passer jeres kalender."
             />
             <Costumerrequest />
-            
+
 
           </div>
         </div>
