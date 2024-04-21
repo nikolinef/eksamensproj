a7f05e9a9309a2d8d1d659f92390865fa4f08b6c
nikolinef <126453544+nikolinef@users.noreply.github.com>
2024-04-21 17:15:48 +0200

    bookingfelter

--- src/components/Costumerrequest.jsx
+++ src/components/Costumerrequest.jsx
@@ -5,21 +5,28 @@ function Costumerrequest() {
         <>
         <div className="costumerrequest">
             <label htmlFor="name">Navn/firmanavn:</label>
+            <br />
             <input type="text" 
             name="navn"
             placeholder="Navn/firmanavn"/>
-
+            <br />
+            <br />
             <label htmlFor="name">Mobilnummer:</label>
+            <br />
             <input type="number" 
             name="mobilnummer"
             placeholder="Mobilnummer"/>
-
+            <br />
+            <br />
             <label htmlFor="name">E-mail:</label>
+            <br />
             <input type="email" 
             name="email"
             placeholder="E-mail"/>
-
+            <br />
+            <br />
             <label htmlFor="name">Besked:</label>
+            <br />
             <textarea type="text" 
             name="besked"
             placeholder="Forespørgsel til mødetidspunkt, dato og evt. sted"/>
